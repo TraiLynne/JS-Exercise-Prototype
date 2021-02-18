ba46a53cb1dd4ec6ff7d7e181c2249392db9f1bf
Trai Lynne <trailynne@gmail.com>
2021-02-17 18:13:22 -0700

    Complete Task 3 Define `Baby` as a child of `Person` Add `favoriteToy` property Pass Person prototype to Baby for method inherit Define `Baby.play` method

--- index.js
+++ index.js
@@ -101,9 +101,16 @@ Car.prototype.drive = function(distance){
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
+function Baby(name, age, favToy) {
+   Person.call(this, name, age, favToy);
+   this.favoriteToy = favToy;
+}
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
  
   
   /* 
